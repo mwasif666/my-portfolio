591bfab4d2ba817d7e1280709ce4b807327c5f97
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-08-03 14:35:17 +0500

    Load About section styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import './scss/bootstrap.scss';
 import './index.css';
 import './banner-themes.css';
 import './loader-background.css';
+import './about-section.css';
 
 createRoot(document.getElementById('root')).render(<App />);
