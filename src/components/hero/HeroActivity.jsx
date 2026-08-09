1b2513ff464ce3b60c1b083de281dc74fc90b78a
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-08-09 21:35:56 +0500

    Use scoped CSS module for hero activity

--- src/components/hero/HeroActivity.jsx
+++ src/components/hero/HeroActivity.jsx
@@ -1,17 +1,11 @@
 import clsx from "clsx";
 import GitHubActivity from "../GitHubActivity";
 import { heroReveal } from "./heroMotion";
+import styles from "./HeroActivity.module.css";
 
 export default function HeroActivity() {
   return (
-    <div
-      className={clsx(
-        heroReveal("delay-[440ms]"),
-        "order-5 w-full",
-        "min-[901px]:absolute min-[901px]:z-[8] min-[901px]:left-[var(--rail)]",
-        "min-[901px]:bottom-[clamp(1.8rem,4vh,3.25rem)] min-[901px]:w-[min(40vw,31rem)]",
-      )}
-    >
+    <div className={clsx(heroReveal("delay-[440ms]"), styles.root)}>
       <GitHubActivity username="mwasif666" />
     </div>
   );
