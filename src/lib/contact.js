c2740c8175aacccd1a361303ded2c4fceb7c002d
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-08-19 20:54:53 +0500

    Add portfolio contact links

---
+++ src/lib/contact.js
@@ -0,0 +1,7 @@
+export const CONTACT = {
+  email: "info@wasifmajeed.com",
+  phoneDisplay: "+92 301 3769247",
+  phoneDigits: "923013769247",
+  whatsappUrl:
+    "https://wa.me/923013769247?text=Hi%20Wasif%2C%20I%27d%20like%20to%20discuss%20a%20project.",
+};
